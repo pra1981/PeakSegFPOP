ca39f1766416175dcb37c7fb2874cebb9eca06fd
Toby Dylan Hocking <tdhock5@gmail.com>
2016-09-23 18:07:17 -0400

    NEWS

--- download.R
+++ download.R
@@ -1,8 +1,8 @@
 bed <- read.table("http://hubs.hpc.mcgill.ca/~thocking/bed/index.txt", header=TRUE)
 "http://hubs.hpc.mcgill.ca/~thocking/genomecov/H3K36me3/McGill0001.bedGraph"
-pre <- "http://hubs.hpc.mcgill.ca/~thocking/genomecov/"
+pre <- "http://hubs.hpc.mcgill.ca/~thocking/genomecovgz/"
 
-bed.file.vec <- paste(bed$file)
+bed.file.vec <- paste(subset(bed, !grepl("Input", file))$file)
 for(bed.file.i in seq_along(bed.file.vec)){
   bed.file <- bed.file.vec[[bed.file.i]]
   sample.id <- dirname(paste(bed.file))
@@ -10,13 +10,14 @@ for(bed.file.i in seq_along(bed.file.vec)){
   bedGraph.file <- paste0(bed.file, "Graph")
   bedGraph.path <- file.path("data", bedGraph.file)
   if(!file.exists(bedGraph.path)){
-    bed.url <- paste0(pre, experiment, "/", sample.id, ".bedGraph")
+    bed.url <- paste0(pre, experiment, "/", sample.id, ".bedGraph.gz")
     cat(sprintf(
       "%4d / %4d %s -> %s\n",
       bed.file.i, length(bed.file.vec),
       bed.url, bedGraph.path))
     dir.create(dirname(bedGraph.path), showWarnings=FALSE, recursive=TRUE)
     download.file(bed.url, bedGraph.path)
+    system(paste("gzip", bedGraph.path))
   }
 }
 
