c3f17cb89335d8f3b57f9fd6637423cc2a71a395
Toby Dylan Hocking <tdhock5@gmail.com>
2016-09-24 19:06:56 -0400

    create sample dirs

--- labels.R
+++ labels.R
@@ -11,3 +11,26 @@ for(regions.RData in regions.RData.vec){
 }
 labels <- do.call(rbind, labels.list)
 save(labels, file="labels.RData")
+
+labels[, chunk.name := paste0(set.name, "/", chunk.id)]
+test.chunk.vec <-
+  c("H3K36me3_AM_immune/11", "H3K36me3_AM_immune/12", "H3K36me3_AM_immune/15", 
+    "H3K36me3_AM_immune/19", "H3K36me3_AM_immune/20", "H3K36me3_AM_immune/9")
+sname <- "H3K36me3_AM_immune"
+train.labels <- labels[set.name==sname & (!chunk.name %in% test.chunk.vec),]
+set.dir <- file.path("labels", paste0(sname, "_folds2-4"))
+train.by.sample <- split(train.labels, train.labels$sample.id, drop=TRUE)
+for(sample.id in names(train.by.sample)){
+  sample.dir <- file.path(set.dir, sample.id)
+  dir.create(sample.dir, showWarnings=FALSE, recursive=TRUE)
+  sample.labels <- train.by.sample[[sample.id]]
+  write.table(
+    sample.labels[, .(chrom, chromStart, chromEnd, annotation)],
+    file.path(sample.dir, "labels.bed"),
+    sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+  coverage.bedGraph <- file.path(sample.dir, "coverage.bedGraph")
+  unlink(coverage.bedGraph)
+  file.symlink(
+    file.path("~/PeakSegFPOP/data", sample.id, "H3K36me3.bedGraph"),
+    coverage.bedGraph)
+}
