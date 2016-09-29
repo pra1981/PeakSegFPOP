e10c24640bc0e1ed9820d62ea08944eefacab235
Toby Dylan Hocking <tdhock5@gmail.com>
2016-09-29 10:04:12 -0400

    test fails for searching target interval only noPeaks or peaks

--- test_cases.R
+++ test_cases.R
@@ -45,55 +45,69 @@ test_that("target interval is around min error", {
   expect_identical(as.integer(min.peaks), 12L)
 })
 
-data("H3K4me3_XJ_immune_chunk1")
+data("H3K4me3_XJ_immune_chunk1", package="coseg")
 sample.id <- "McGill0106"
 H3K4me3_XJ_immune_chunk1$count <- H3K4me3_XJ_immune_chunk1$coverage
 by.sample <-
   split(H3K4me3_XJ_immune_chunk1, H3K4me3_XJ_immune_chunk1$sample.id)
 one.sample <- by.sample[[sample.id]]
 one.sample$chrom <- "chr1"
-labels <- data.frame(
-  chrom="chr1",
-  chromStart=20007000,
-  chromEnd=20009000,
-  annotation="peakStart")
-sample.dir <- "test/small"
-unlink(sample.dir, recursive=TRUE)
-dir.create(sample.dir, showWarnings=FALSE, recursive=TRUE)
-coverage.bedGraph <- file.path(sample.dir, "coverage.bedGraph")
-write.table(
-  one.sample[, c("chrom", "chromStart", "chromEnd", "coverage")],
-  coverage.bedGraph,
-  quote=FALSE, sep="\t",
-  row.names=FALSE, col.names=FALSE)
-labels.bed <- file.path(sample.dir, "labels.bed")
-write.table(
-  labels, labels.bed,
-  quote=FALSE, sep="\t",
-  row.names=FALSE, col.names=FALSE)
-
-test.cmd <- paste("Rscript create_problems.R hg19_problems.bed", sample.dir)
-system(test.cmd)
-labels.bed <- Sys.glob(paste0(sample.dir, "/problems/*/labels.bed"))
-problem.dir <- dirname(labels.bed)
-sh.path <- file.path(problem.dir, "coverage.bedGraph.sh")
-bash.cmd <- paste("bash", sh.path)
-system(bash.cmd)
-target.tsv <- file.path(problem.dir, "target.tsv")
-target.vec <- scan(target.tsv, quiet=TRUE)
-loss <- fread(paste0("cat ", problem.dir, "/*_loss.tsv"))
-setnames(loss, c("penalty", "segments", "peaks", "bases", "mean.pen.cost", "total.cost", "status", "mean.intervals", "max.intervals"))
 n.bases <- with(one.sample, sum(chromEnd-chromStart))
 exp.bases <- rep(n.bases, l=nrow(loss))
+labels.list <- list(
+  peakStart=data.frame(
+    chrom="chr1",
+    chromStart=20007000,
+    chromEnd=20009000,
+    annotation="peakStart"),
+  peaks=data.frame(
+    chrom="chr1",
+    chromStart=20007000,
+    chromEnd=20009000,
+    annotation="peaks"),
+  noPeaks=data.frame(
+    chrom="chr1",
+    chromStart=20005000,
+    chromEnd=20007000,
+    annotation="noPeaks"))
 
-test_that("PeakSegFPOP reports correct number of bases", {
-  expect_identical(as.integer(loss$bases), as.integer(exp.bases))
-})
+results.list <- list()
+for(labels.name in names(labels.list)){
+  sample.dir <- file.path("test", labels.name)
+  labels <- labels.list[[labels.name]]
+  unlink(sample.dir, recursive=TRUE)
+  dir.create(sample.dir, showWarnings=FALSE, recursive=TRUE)
+  coverage.bedGraph <- file.path(sample.dir, "coverage.bedGraph")
+  write.table(
+    one.sample[, c("chrom", "chromStart", "chromEnd", "coverage")],
+    coverage.bedGraph,
+    quote=FALSE, sep="\t",
+    row.names=FALSE, col.names=FALSE)
+  labels.bed <- file.path(sample.dir, "labels.bed")
+  write.table(
+    labels, labels.bed,
+    quote=FALSE, sep="\t",
+    row.names=FALSE, col.names=FALSE)
+  test.cmd <- paste("Rscript create_problems.R hg19_problems.bed", sample.dir)
+  system(test.cmd)
+  labels.bed <- Sys.glob(paste0(sample.dir, "/problems/*/labels.bed"))
+  problem.dir <- dirname(labels.bed)
+  sh.path <- file.path(problem.dir, "coverage.bedGraph.sh")
+  bash.cmd <- paste("bash", sh.path)
+  system(bash.cmd)
+  target.tsv <- file.path(problem.dir, "target.tsv")
+  target.vec <- scan(target.tsv, quiet=TRUE)
+  loss <- fread(paste0("cat ", problem.dir, "/*_loss.tsv"))
+  setnames(loss, c("penalty", "segments", "peaks", "bases", "mean.pen.cost", "total.cost", "status", "mean.intervals", "max.intervals"))  
+  test_that("PeakSegFPOP reports correct number of bases", {
+    expect_identical(as.integer(loss$bases), as.integer(exp.bases))
+  })
+  loss.ord <- loss[order(penalty),]
+  loss.ord[, log.penalty := log(penalty)]
+  results.list[[labels.name]] <-
+    loss.ord[target.vec[1] < log.penalty & log.penalty < target.vec[2],]
+}
 
-loss.ord <- loss[order(penalty),]
-loss.ord[, log.penalty := log(penalty)]
-best <- loss.ord[target.vec[1] < log.penalty & log.penalty < target.vec[2],]
-
-test_that("Target interval contains one model with 1 peak", {
-  expect_identical(as.integer(best$peaks), 1L)
+test_that("Target interval contains one model with 1 peak for peakStart label", {
+  expect_identical(as.integer(results.list$peakStart$peaks), 1L)
 })
