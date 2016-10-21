f7756d42a3b133196149bbc9040b1a680a01501e
Toby Dylan Hocking <tdhock5@gmail.com>
2016-10-21 09:40:30 -0400

    use create_problems_all.R

--- pipeline.R
+++ pipeline.R
@@ -17,48 +17,29 @@ convert.cmd <- paste("Rscript convert_labels.R", set.dir)
 system.or.stop(convert.cmd)
 
 ## Create problems for each sample.
-samples.dir <- file.path(set.dir, "samples")
-sample.dir.vec <- Sys.glob(file.path(samples.dir, "*", "*"))
-problems.bed <- file.path(set.dir, "problems.bed")
-for(sample.dir in sample.dir.vec){
-  create.cmd <- paste("Rscript create_problems_sample.R", problems.bed, sample.dir)
-  system.or.stop(create.cmd)
-}
+create.cmd <- paste("Rscript create_problems_all.R", set.dir)
+system.or.stop(create.cmd)
 
 ## Compute target interval for each problem.
+samples.dir <- file.path(set.dir, "samples")
 labels.bed.vec <- Sys.glob(file.path(
   samples.dir, "*", "*", "problems", "*", "labels.bed"))
 for(labels.bed in labels.bed.vec){
   sample.dir <- dirname(labels.bed)
-  create.cmd <- paste("Rscript compute_coverage_target.R", sample.dir)
-  system.or.stop(create.cmd)
+  target.cmd <- paste("Rscript compute_coverage_target.R", sample.dir)
+  system.or.stop(target.cmd)
 }
 
 ## Train single-sample model.
 model.RData <- file.path(set.dir, "model.RData")
-train.cmd <- paste("Rscript train_model.R", samples.dir, model.RData)
+train.cmd <- paste("Rscript train_model.R", set.dir)
 system.or.stop(train.cmd)
 
-## Single-sample prediction, one job for each problem. Or would it be
-## better to create one job for each sample? Or a few jobs for each
-## sample?
-## library(data.table)
-## problems <- fread("hg19_problems.bed")
-## setnames(problems, c("chrom", "chromStart", "chromEnd"))
-## problems[, bases := chromEnd-chromStart]
-## problems[, cum.bases := cumsum(bases)]
-## n.jobs <- length(labels.bed.vec)
-## bases.per.job <- sum(problems$bases)/n.jobs
-## problems[, job.id := cum.bases %/% bases.per.job]
-## problems[, list(bases=sum(bases)), by=job.id]
-## problem.dir.vec <- Sys.glob(file.path(
-##   samples.dir, "*", "*", "problems", "*"))
-## for(problem.dir in problem.dir.vec){
-##   predict.cmd <- paste("Rscript predict_problem.R", model.RData, problem.dir)
-##   system.or.stop(predict.cmd)
-## }
-for(sample.dir in sample.dir.vec){
-  predict.cmd <- paste("Rscript predict_sample.R", model.RData, sample.dir)
+## Single-sample prediction, one job for each problem.
+sh.vec <- Sys.glob(file.path(
+  set.dir, "problems", "*", "jointProblems.bed.sh"))
+for(sh in sh.vec){
+  predict.cmd <- paste("bash", sh)
   system.or.stop(predict.cmd)
 }
 
