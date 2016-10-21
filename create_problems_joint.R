035c0d110d16b6adb310e70ad5865f6dcbf36821
Toby Dylan Hocking <tdhock5@gmail.com>
2016-10-21 09:39:47 -0400

    handle no peaks predicted

--- create_problems_joint.R
+++ create_problems_joint.R
@@ -20,6 +20,9 @@ arg.vec <- c(
 arg.vec <- c(
   "test/H3K4me3_TDH_other/samples",
   "7")
+arg.vec <- c(
+  "/home/tdhock/PeakSegFPOP/test/input/samples",
+  "chr10:125919472-128616069")
 
 arg.vec <- commandArgs(trailingOnly=TRUE)
 
@@ -29,8 +32,10 @@ problem.name <- arg.vec[2]
 library(data.table)
 library(PeakSegJoint)#for clusterPeaks
 
-peaks.bed.vec <- Sys.glob(file.path(
-  samples.dir, "*", "*", "problems", problem.name, "peaks.bed"))
+peaks.glob <- file.path(
+  samples.dir, "*", "*", "problems", problem.name, "peaks.bed")
+peaks.bed.vec <- Sys.glob(peaks.glob)
+cat("Found", length(peaks.bed.vec), peaks.glob, "files.\n")
 peaks.list <- list()
 for(sample.i in seq_along(peaks.bed.vec)){
   peaks.bed <- peaks.bed.vec[[sample.i]]
@@ -51,15 +56,20 @@ for(sample.i in seq_along(peaks.bed.vec)){
   })
 }
 peaks <- do.call(rbind, peaks.list)
-clustered <- clusterPeaks(peaks)
-clusters <- data.table(clustered)[, list(
-  clusterStart=min(chromStart),
-  clusterEnd=max(chromEnd)
+problems.list <- if(is.data.frame(peaks) && 0 < nrow(peaks)){
+  clustered <- clusterPeaks(peaks)
+  clusters <- data.table(clustered)[, list(
+    clusterStart=min(chromStart),
+    clusterEnd=max(chromEnd)
   ), by=cluster]
-clusters[, clusterStart1 := clusterStart + 1L]
-setkey(clusters, clusterStart1, clusterEnd)
-problems.list <- list(
-  peaks=clusters[, .(clusterStart, clusterEnd)])
+  clusters[, clusterStart1 := clusterStart + 1L]
+  setkey(clusters, clusterStart1, clusterEnd)
+  list(
+    peaks=clusters[, .(clusterStart, clusterEnd)])
+}else{
+  cat("No predicted peaks.\n")
+  list()
+}
 
 labels.bed.vec <- Sys.glob(file.path(
   samples.dir, "*", "*", "problems", problem.name, "labels.bed"))
@@ -80,7 +90,9 @@ for(sample.i in seq_along(labels.bed.vec)){
     data.table(sample.id, sample.group, sample.labels)
 }
 labels <- do.call(rbind, labels.list)
-if(!is.null(labels)){
+if(is.null(labels)){
+  cat("No labels.\n")
+}else{
   label.props <- labels[, list(
     prop.noPeaks=mean(annotation=="noPeaks")
     ), by=.(labelStart, labelEnd)]
@@ -90,161 +102,169 @@ if(!is.null(labels)){
   labels.with.no.peaks <- over[is.na(cluster),]
   labels.with.no.peaks[, bases := labelEnd - labelStart]
   labels.with.no.peaks[, reduce := as.integer(bases/3)]
+  cat(
+    "Found", nrow(labels.with.no.peaks),
+    "labeled regions with no peaks out of",
+    nrow(labels), "total.\n")
   problems.list$labels <- labels.with.no.peaks[, data.table(
     clusterStart=labelStart+reduce,
     clusterEnd=labelEnd-reduce)]
 }
 
 problems <- do.call(rbind, problems.list)
-setkey(problems, clusterStart, clusterEnd)
-problems[, bases := clusterEnd - clusterStart]
-mid.between.problems <- problems[, as.integer((clusterEnd[-.N]+clusterStart[-1])/2)]
-problems[, mid.before := c(NA_integer_, mid.between.problems)]
-problems[, mid.after := c(mid.between.problems, NA_integer_)]
-problems[, problemStart := as.integer(clusterStart-bases)]
-problems[, problemEnd := as.integer(clusterEnd+bases)]
-problems[problemStart < mid.before, problemStart := mid.before]
-problems[mid.after < problemEnd, problemEnd := mid.after]
-chrom <- peaks$chrom[1]
-problem.info <- problems[, data.table(
-  problemStart,
-  problemEnd,
-  problem.name=sprintf("%s:%d-%d", chrom, problemStart, problemEnd))]
-problem.info[, problemStart1 := problemStart + 1L]
-setkey(problem.info, problemStart1, problemEnd)
+if(is.data.table(problems) && 0 < nrow(problems)){
+  setkey(problems, clusterStart, clusterEnd)
+  problems[, bases := clusterEnd - clusterStart]
+  mid.between.problems <- problems[, as.integer((clusterEnd[-.N]+clusterStart[-1])/2)]
+  problems[, mid.before := c(NA_integer_, mid.between.problems)]
+  problems[, mid.after := c(mid.between.problems, NA_integer_)]
+  problems[, problemStart := as.integer(clusterStart-bases)]
+  problems[, problemEnd := as.integer(clusterEnd+bases)]
+  problems[problemStart < mid.before, problemStart := mid.before]
+  problems[mid.after < problemEnd, problemEnd := mid.after]
+  chrom <- peaks$chrom[1]
+  problem.info <- problems[, data.table(
+    problemStart,
+    problemEnd,
+    problem.name=sprintf("%s:%d-%d", chrom, problemStart, problemEnd))]
+  problem.info[, problemStart1 := problemStart + 1L]
+  setkey(problem.info, problemStart1, problemEnd)
 
-if(!is.null(labels)){
-  labels[, labelStart1 := labelStart + 1L]
-  setkey(labels, labelStart1, labelEnd)
-  problems.with.labels <- foverlaps(problem.info, labels, nomatch=0L)
-  setkey(problems.with.labels, problem.name)
-}
-
-coverage.bedGraph.vec <- Sys.glob(file.path(
-  samples.dir, "*", "*", "problems", problem.name, "coverage.bedGraph"))
-if(FALSE){
-  
-  coverage.list <- list()
-  for(coverage.bedGraph in coverage.bedGraph.vec){
-    problem.dir <- dirname(coverage.bedGraph)
-    problems.dir <- dirname(problem.dir)
-    sample.dir <- dirname(problems.dir)
-    sample.id <- basename(sample.dir)
-    group.dir <- dirname(sample.dir)
-    sample.group <- basename(group.dir)
-    sample.cov <- fread(coverage.bedGraph)
-    setnames(
-      sample.cov,
-      c("chrom", "chromStart", "chromEnd", "count"))
-    coverage.list[[coverage.bedGraph]] <- 
-      data.table(sample.id, sample.group, sample.cov)
+  if(!is.null(labels)){
+    labels[, labelStart1 := labelStart + 1L]
+    setkey(labels, labelStart1, labelEnd)
+    problems.with.labels <- foverlaps(problem.info, labels, nomatch=0L)
+    setkey(problems.with.labels, problem.name)
   }
-  coverage <- do.call(rbind, coverage.list)
 
-  ann.colors <-
-    c(noPeaks="#f6f4bf",
-      peakStart="#ffafaf",
-      peakEnd="#ff4c4c",
-      peaks="#a445ee")
-  ggplot()+
-    theme_bw()+
-    theme(panel.margin=grid::unit(0, "lines"))+
-    facet_grid(sample.id ~ ., scales="free")+
-    scale_fill_manual(values=ann.colors)+
-    geom_tallrect(aes(
-      xmin=labelStart/1e3,
-      xmax=labelEnd/1e3,
-      fill=annotation),
-      color="grey",
-      alpha=0.5,
-      data=labels)+
-    geom_vline(aes(xintercept=problemStart/1e3),
-               data=problems,
-               color="red")+
-    geom_segment(aes(problemStart/1e3, 0,
-                     xend=problemEnd/1e3, yend=0),
-                 color="red",
-                 size=3,
-                 data=problems)+
-    geom_step(aes(chromStart/1e3, count),
-              data=coverage,
-              color="grey50")+
-    geom_segment(aes(chromStart/1e3, 0,
-                     xend=chromEnd/1e3, yend=0),
-                 data=peaks,
-                 color="deepskyblue",
-                 size=2)
-  
-}
+  coverage.bedGraph.vec <- Sys.glob(file.path(
+    samples.dir, "*", "*", "problems", problem.name, "coverage.bedGraph"))
+  if(FALSE){
+    
+    coverage.list <- list()
+    for(coverage.bedGraph in coverage.bedGraph.vec){
+      problem.dir <- dirname(coverage.bedGraph)
+      problems.dir <- dirname(problem.dir)
+      sample.dir <- dirname(problems.dir)
+      sample.id <- basename(sample.dir)
+      group.dir <- dirname(sample.dir)
+      sample.group <- basename(group.dir)
+      sample.cov <- fread(coverage.bedGraph)
+      setnames(
+        sample.cov,
+        c("chrom", "chromStart", "chromEnd", "count"))
+      coverage.list[[coverage.bedGraph]] <- 
+        data.table(sample.id, sample.group, sample.cov)
+    }
+    coverage <- do.call(rbind, coverage.list)
 
-data.dir <- dirname(samples.dir)
-jointProblems <- file.path(
-  data.dir, "problems", problem.name, "jointProblems")
-joint.model.RData <- file.path(data.dir, "joint.model.RData")
-## TODO make directories with sh files for each.
-makeProblem <- function(problem.i){
-  problem <- problem.info[problem.i,]
-  pname <- problem$problem.name
-  ##cat(sprintf("%4d / %4d problems %s\n", problem.i, nrow(problem.info), pname))
-  problem.dir <- file.path(jointProblems, pname)
-  dir.create(problem.dir, showWarnings=FALSE, recursive=TRUE)
-  pout <- data.table(
-    chrom,
-    problem[, .(problemStart, problemEnd)],
-    problem.name)
-  write.table(
-    pout,
-    file.path(problem.dir, "problem.bed"),
-    quote=FALSE,
-    sep="\t",
-    row.names=FALSE,
-    col.names=FALSE)
-  if(!is.null(labels) && pname %in% problems.with.labels$problem.name){
-    problem.labels <- problems.with.labels[pname]
+    ann.colors <-
+      c(noPeaks="#f6f4bf",
+        peakStart="#ffafaf",
+        peakEnd="#ff4c4c",
+        peaks="#a445ee")
+    ggplot()+
+      theme_bw()+
+      theme(panel.margin=grid::unit(0, "lines"))+
+      facet_grid(sample.id ~ ., scales="free")+
+      scale_fill_manual(values=ann.colors)+
+      geom_tallrect(aes(
+        xmin=labelStart/1e3,
+        xmax=labelEnd/1e3,
+        fill=annotation),
+        color="grey",
+        alpha=0.5,
+        data=labels)+
+      geom_vline(aes(xintercept=problemStart/1e3),
+                 data=problems,
+                 color="red")+
+      geom_segment(aes(problemStart/1e3, 0,
+                       xend=problemEnd/1e3, yend=0),
+                   color="red",
+                   size=3,
+                   data=problems)+
+      geom_step(aes(chromStart/1e3, count),
+                data=coverage,
+                color="grey50")+
+      geom_segment(aes(chromStart/1e3, 0,
+                       xend=chromEnd/1e3, yend=0),
+                   data=peaks,
+                   color="deepskyblue",
+                   size=2)
+    
+  }
+
+  data.dir <- dirname(samples.dir)
+  jointProblems <- file.path(
+    data.dir, "problems", problem.name, "jointProblems")
+  joint.model.RData <- file.path(data.dir, "joint.model.RData")
+  ## TODO make directories with sh files for each.
+  makeProblem <- function(problem.i){
+    problem <- problem.info[problem.i,]
+    pname <- problem$problem.name
+    ##cat(sprintf("%4d / %4d problems %s\n", problem.i, nrow(problem.info), pname))
+    problem.dir <- file.path(jointProblems, pname)
+    dir.create(problem.dir, showWarnings=FALSE, recursive=TRUE)
+    pout <- data.table(
+      chrom,
+      problem[, .(problemStart, problemEnd)],
+      problem.name)
     write.table(
-      problem.labels[, .(
-        chrom, labelStart, labelEnd, annotation, sample.id, sample.group)],
-      file.path(problem.dir, "labels.tsv"),
+      pout,
+      file.path(problem.dir, "problem.bed"),
       quote=FALSE,
       sep="\t",
       row.names=FALSE,
       col.names=FALSE)
-    ## Script for target.
-    target.tsv <- file.path(problem.dir, "target.tsv")
-    sh.file <- paste0(target.tsv, ".sh")
-    script.txt <- paste0(PBS.header, "
+    if(!is.null(labels) && pname %in% problems.with.labels$problem.name){
+      problem.labels <- problems.with.labels[pname]
+      write.table(
+        problem.labels[, .(
+          chrom, labelStart, labelEnd, annotation, sample.id, sample.group)],
+        file.path(problem.dir, "labels.tsv"),
+        quote=FALSE,
+        sep="\t",
+        row.names=FALSE,
+        col.names=FALSE)
+      ## Script for target.
+      target.tsv <- file.path(problem.dir, "target.tsv")
+      sh.file <- paste0(target.tsv, ".sh")
+      script.txt <- paste0(PBS.header, "
 #PBS -o ", target.tsv, ".out
 #PBS -e ", target.tsv, ".err
 #PBS -N JTarget", pname, "
 ", "Rscript ", normalizePath("compute_joint_target.R", mustWork=TRUE), " ",
 problem.dir, " 
 ")
-    writeLines(script.txt, sh.file)
-  }
-  ## Script for peaks.
-  peaks.bed <- file.path(problem.dir, "peaks.bed")
-  sh.file <- paste0(peaks.bed, ".sh")
-  script.txt <- paste0(PBS.header, "
+      writeLines(script.txt, sh.file)
+    }
+    ## Script for peaks.
+    peaks.bed <- file.path(problem.dir, "peaks.bed")
+    sh.file <- paste0(peaks.bed, ".sh")
+    script.txt <- paste0(PBS.header, "
 #PBS -o ", peaks.bed, ".out
 #PBS -e ", peaks.bed, ".err
 #PBS -N JPred", problem$problem.name, "
 ", "Rscript ", normalizePath("predict_problem_joint.R", mustWork=TRUE), " ",
 joint.model.RData, " ", problem.dir, " 
 ")
-  writeLines(script.txt, sh.file)
+    writeLines(script.txt, sh.file)
+  }
+
+  cat(
+    "Creating ", nrow(problem.info),
+    " joint segmentation problems for ", problem.name,
+    "\n", sep="")
+  library(parallel)
+  nothing <- lapply(1:nrow(problem.info), makeProblem)
+
+  write.table(
+    problem.info[, .(chrom, problemStart, problemEnd)],
+    paste0(jointProblems, ".bed"),
+    quote=FALSE,
+    sep="\t",
+    col.names=FALSE,
+    row.names=FALSE)
+}else{
+  cat("No joint problems.\n")
 }
-
-cat(
-  "Creating ", nrow(problem.info),
-  " joint segmentation problems for ", problem.name,
-  "\n", sep="")
-library(parallel)
-nothing <- lapply(1:nrow(problem.info), makeProblem)
-
-write.table(
-  problem.info[, .(chrom, problemStart, problemEnd)],
-  paste0(jointProblems, ".bed"),
-  quote=FALSE,
-  sep="\t",
-  col.names=FALSE,
-  row.names=FALSE)
