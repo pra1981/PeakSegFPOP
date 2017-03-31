d5882cff4af5356b876b8731b870099fa09f3adb
Toby Dylan Hocking <tdhock5@gmail.com>
2017-03-31 14:35:32 -0400

    upgrade coseg

--- packages.R
+++ packages.R
@@ -66,10 +66,9 @@ works_with_R(
   doParallel="1.0.6",
   geometry="0.3.6",#for penaltyLearning.
   data.table="1.10.4",
-  ##"Rdatatable/data.table@7515fbe6c6f60114da72067db44fbe78ecdbd8fb",
   "tdhock/penaltyLearning@126e37ac8634499450d32f3da961464a6ebed4ec",
   "tdhock/PeakError@b0f0b4edc413176ebb183fc68f1504c9d86e3ef7",
-  "tdhock/coseg@98fba1846fb10c1a8ae83778991d4a55e1f85448",
+  "tdhock/coseg@49d056963d8c0eaebea22fef453842255a330a05",
   "tdhock/animint@78974d8788930034109289e42f8c90f1ee804290",
   "tdhock/PeakSegJoint@a02c58314a0466774011def72c14d1c61460e0e9",
   "tdhock/cosegData@83e6f787bf4f9f9ec7d299bb9e6b32db522021a2",
