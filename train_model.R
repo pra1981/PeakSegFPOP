c486d1b977c2be2faa1af20b16455cdfaea098de
Toby Dylan Hocking <tdhock5@gmail.com>
2017-06-13 08:34:42 -0400

    save train.mean.vec for separate model

--- train_model.R
+++ train_model.R
@@ -51,6 +51,7 @@ model <- if(nrow(features) < 10){
     min.observations=nrow(features),
     reg.type=ifelse(nrow(features) < 20, "1sd", "min(mean)"))
 }
+model$train.mean.vec <- colMeans(features)
 
 cat("Learned regularization parameter and weights:\n")
 print(model$pred.param.mat)
